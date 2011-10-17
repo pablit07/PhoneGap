983daac8f0ab9f57c68b4ad08e55fb878f00b690
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2011-10-17 13:30:58 -0600

    add contacts search

--- js/main.js
+++ js/main.js
@@ -31,6 +31,8 @@ function App() {
             console.warn("Warning: Action " + action + " is not defined");
         }
 
+        return false;
+
     });
 
     this.$results = $('div#Results');
@@ -40,8 +42,6 @@ function App() {
 App.prototype.action_changeGrid = function() {
     this.$body.toggleClass('alignedLeft');
     this.$body.toggleClass('grid');
-
-    return false;
 }
 
 App.prototype.action_accelerometer = function() {
@@ -50,7 +50,7 @@ App.prototype.action_accelerometer = function() {
 
     this.$results.html(_text);
 
-    var threshold = 1;
+    var threshold = 3;
 
     function onSuccess(acceleration) {
 
@@ -76,24 +76,39 @@ App.prototype.action_accelerometer = function() {
     var options = { frequency: 100 };
 
     var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-
-    return false;
 }
 
 App.prototype.action_vibrate = function() {
     navigator.notification.vibrate(2000);
-
-    return false;
 }
 
 App.prototype.action_ajax = function() {
     this.$results.load("http://wallst.com/business.asp");
-
-    return false;
 }
 
 App.prototype.action_custom1 = function() {
-    alert('5');
+    
+    var self = this;
+    
+    var dialog = new Dialog([{displayName: "Enter contact search term: ", name: 'name'}], {callback: function(oResults) {
+        
+        function onSuccess(rContacts) {
+            self.$results.html(rContacts.length);
+        };
+        
+        function onError() {
+            alert("on error");
+        }
+        
+        var options = new ContactFindOptions();
+        options.filter = oResults.name;
+        var fields = ["displayName", "name"];
+        
+        navigator.contacts.find(fields, onSuccess, onError, options);
+        
+    }});
+    dialog.init(this.$body);
+    
 }
 
 App.prototype.action_custom2 = function() {
@@ -109,11 +124,55 @@ function Dialog(rInputs, oArgs) {
 
     var rInputs = rInputs || [];
     var oArgs = oArgs || {};
+    
+    var self = this;
+    
+    this.callback = oArgs.callback;
+    
+    this.$dialog = $("<div class='dialog'><form></form></div>");
 
-    $.each(rInputs, function(i) {
-        
-    })
+    $.each(rInputs, function(i, e) {
+        self.$dialog.append($("<div class='field'>"+e.displayName+"</div>").append($("<input type='text' name='"+e.name+"' />")));
+    });
+    
+    this.$dialog.append($("<input type='button' value='Submit' />").click(function() {
+        self.submitDialog();
+    }));
+    
+    this.$dialog.append($("<input type='button' value='Cancel' />").click(function() {
+        self.closeDialog();
+    }));
 }
+
+Dialog.prototype.init = function($parent) {
+    
+
+    this.$dialog.css('top', (($parent.height() / 2) - (this.$dialog.height() / 2)) + 'px');
+    this.$dialog.css('left', (($parent.width() / 2) - (this.$dialog.width() / 2)) + 'px');
+    
+    $parent.append(this.$dialog);
+}
+
+Dialog.prototype.closeDialog = function() {
+    this.$dialog.remove();
+}
+    
+Dialog.prototype.submitDialog = function() {
+    
+    var rResult = {};
+    
+    $('input[type="text"]', this.$dialog[0]).each(function(i, e) {
+        var $e = $(e);
+        rResult[$e.attr('name')] = $e.val();
+    });
+    
+    if (this.callback && typeof this.callback == 'function') {
+        this.callback(rResult);
+    }
+    
+    this.closeDialog();
+}
+    
 
 var _text = "<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>";
 
